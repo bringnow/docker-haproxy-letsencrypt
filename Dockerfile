27b7a94ebf1d95638fe85d5e445583721c5637a5
Fabian Köster <koesterreich@fastmail.fm>
2016-01-20 12:52:48 +0100

    Initial Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM camptocamp/haproxy-luasec
+MAINTAINER bringnow team <wecare@bringnow.com>
+
+ENV ACME_PLUGIN_VERSION 0.1.1
+
+RUN buildDeps='curl ca-certificates' \
+	&& apt-get update && apt-get install -y $buildDeps --no-install-recommends \
+	&& curl -SL https://github.com/janeczku/haproxy-acme-validation-plugin/archive/${ACME_PLUGIN_VERSION}.tar.gz -o acme-plugin.tar.gz \
+	&& tar xzf acme-plugin.tar.gz --strip-components=1 --no-anchored acme-http01-webroot.lua -C /etc/haproxy/ \
+	&& rm *.tar.gz \
+	&& apt-get purge -y --auto-remove $buildDeps
+
+EXPOSE 80 443
+
+COPY entrypoint.sh ./
+
+VOLUME /etc/letsencrypt
+VOLUME /var/acme-webroot
+
+ENTRYPOINT ["entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# Based on https://github.com/million12/docker-haproxy/blob/master/container-files/bootstrap.sh
+
+HAPROXY_CONFIG="/etc/haproxy/haproxy.cfg"
+
+set -u
+
+# User params
+HAPROXY_USER_PARAMS=$@
+
+# Internal params
+HAPROXY_PID_FILE="/var/run/haproxy.pid"
+HAPROXY_CMD="haproxy -f ${HAPROXY_CONFIG} ${HAPROXY_USER_PARAMS} -D -p ${HAPROXY_PID_FILE}"
+HAPROXY_CHECK_CONFIG_CMD="haproxy -f ${HAPROXY_CONFIG} -c"
+
+
+#######################################
+# Echo/log function
+# Arguments:
+#   String: value to log
+#######################################
+log() {
+  if [[ "$@" ]]; then echo "[`date +'%Y-%m-%d %T'`] $@";
+  else echo; fi
+}
+
+#######################################
+# Dump current $HAPROXY_CONFIG
+#######################################
+print_config() {
+  log "Current HAProxy config $HAPROXY_CONFIG:"
+  printf '=%.0s' {1..100} && echo
+  cat $HAPROXY_CONFIG
+  printf '=%.0s' {1..100} && echo
+}
+
+
+# Launch HAProxy.
+log $HAPROXY_CMD && print_config
+$HAPROXY_CHECK_CONFIG_CMD
+$HAPROXY_CMD
+# Exit immidiately in case of any errors or when we have interactive terminal
+if [[ $? != 0 ]] || test -t 0; then exit $?; fi
+log "HAProxy started with $HAPROXY_CONFIG config, pid $(cat $HAPROXY_PID_FILE)." && log
+
+# Check if config or certificates were changed
+while inotifywait -q -r $HAPROXY_CONFIG /etc/letsencrypt; do
+  if [ -f $HAPROXY_PID_FILE ]; then
+    log "Restarting HAProxy due to config changes..." && print_config
+    $HAPROXY_CHECK_CONFIG_CMD
+    $HAPROXY_CMD -sf $(cat $HAPROXY_PID_FILE)
+    log "HAProxy restarted, pid $(cat $HAPROXY_PID_FILE)." && log
+  else
+    log "Error: no $HAPROXY_PID_FILE present, HAProxy exited."
+    break
+  fi
+done
